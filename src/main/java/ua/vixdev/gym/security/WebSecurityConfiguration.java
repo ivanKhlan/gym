c111515d143af1608d3021c3905bef8c2972297d
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-02-23 18:58:26 +0100

    Create cesurity config

---
+++ src/main/java/ua/vixdev/gym/security/WebSecurityConfiguration.java
@@ -0,0 +1,53 @@
+package ua.vixdev.gym.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalAuthentication()
+public class WebSecurityConfiguration {
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/users/**").authenticated()
+                        .anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    protected InMemoryUserDetailsManager configureAuthentication() {
+        List<UserDetails> users = new ArrayList<>();
+        List<GrantedAuthority> adminAuthority = new ArrayList<>();
+
+        adminAuthority.add(new SimpleGrantedAuthority("ADMIN"));
+        UserDetails admin = new User("admin", "{noop}pass", adminAuthority);
+        users.add(admin);
+
+        List<GrantedAuthority> userAuthority = new ArrayList<>();
+        adminAuthority.add(new SimpleGrantedAuthority("USER"));
+        UserDetails user = new User("user", "{noop}pass", userAuthority);
+        users.add(user);
+
+        return new InMemoryUserDetailsManager(users);
+    }
+}
