f966a32a3a19c0196671d9ba7c76957594b56fa3
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-03-01 23:42:15 +0100

    Separate code

--- src/main/java/ua/vixdev/gym/security/service/ResetPasswordService.java
+++
@@ -1,8 +0,0 @@
-package ua.vixdev.gym.security.service;
-
-import ua.vixdev.gym.security.controller.dto.ChangePasswordDto;
-
-public interface ResetPasswordService {
-
-    void changePassword(Long id, ChangePasswordDto changePassword);
-}
